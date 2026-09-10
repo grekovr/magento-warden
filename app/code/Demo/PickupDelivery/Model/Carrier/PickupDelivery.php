4abf8b78e62ce732c21502eddd6c51ee1d0f7701
Ruslan Hrekov <grekovr@gmail.com>
2026-09-10 14:06:48 +0300

    Add PickupDelivery shipping carrier

---
+++ app/code/Demo/PickupDelivery/Model/Carrier/PickupDelivery.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Demo\PickupDelivery\Model\Carrier;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory;
+use Magento\Quote\Model\Quote\Address\RateResult\MethodFactory;
+use Magento\Shipping\Model\Carrier\AbstractCarrier;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+use Magento\Shipping\Model\Rate\ResultFactory;
+use Psr\Log\LoggerInterface;
+
+class PickupDelivery extends AbstractCarrier implements CarrierInterface
+{
+    protected $_code = 'pickupdelivery';
+
+    protected $_isFixed = true;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        ErrorFactory $rateErrorFactory,
+        LoggerInterface $logger,
+        private readonly ResultFactory $rateResultFactory,
+        private readonly MethodFactory $rateMethodFactory,
+        array $data = []
+    ) {
+        parent::__construct(
+            $scopeConfig,
+            $rateErrorFactory,
+            $logger,
+            $data
+        );
+    }
+
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        $result = $this->rateResultFactory->create();
+        $method = $this->rateMethodFactory->create();
+        $price = (float) $this->getConfigData('price');
+
+        $method->setCarrier($this->_code);
+        $method->setCarrierTitle($this->getConfigData('title'));
+        $method->setMethod($this->_code);
+        $method->setMethodTitle($this->getConfigData('name'));
+        $method->setPrice($price);
+        $method->setCost($price);
+
+        $result->append($method);
+
+        return $result;
+    }
+
+    public function getAllowedMethods()
+    {
+        return [
+            $this->_code => $this->getConfigData('name'),
+        ];
+    }
+}
